111e456d164dcf4f60da039f7cb3e992bb11a5f1
mrHoft <ru.hoft@gmail.com>
2025-01-28 11:14:58 +0300

    feat: db checks

--- server/db/checks.ts
+++ server/db/checks.ts
@@ -1,105 +1,77 @@
 import db from '~/server/utils/pool.ts'
 import { createAdminUserToken } from '~/server/utils/users.ts'
 import { ERROR_CODES } from '~/server/db/codes.ts'
+import { initTableCounters, initTableUsers, initTriggers } from '~/server/db/init.ts'
 
-async function createCountersTable() {
-  console.log('No counters table found: creating one...')
+const createCountersTable = () =>
+  db.pool.query(initTableCounters).then(() => {
+    console.log('Create counters table: Success!')
+  }).catch((err) => {
+    console.log('Create counters table: Failed:', err.message)
+  }).then(() => 1)
 
-  await db.pool.query(/* sql */ `
-    CREATE TABLE IF NOT EXISTS counters (
-      id serial PRIMARY KEY,
-      name varchar(25) NOT NULL UNIQUE,
-      value int4 NOT NULL DEFAULT 0,
-      created_at timestamptz NOT NULL DEFAULT now(),
-      created_by varchar(25) NOT NULL DEFAULT 'default'
-    );`)
-}
-
-async function createDefaultCounters() {
-  console.log('Creating default counters...')
-
-  const { rows } = await db.pool.query(/* sql */ `
-    INSERT INTO counters (name) VALUES ('default'), ('counter-example') ON CONFLICT DO NOTHING RETURNING *;`)
-  if (rows.length) console.log('Success!')
-}
-
-function checkCountersTable() {
+const checkCountersTable = () =>
   db.pool
     .query<Record<'name', string>>('SELECT name FROM counters;')
     .then(({ rows }) => {
       console.log('Available counters:', rows.map(({ name }) => name).join(', '))
+      return 0
     })
     .catch(async (error) => {
       if (error.code === ERROR_CODES.relation) {
-        try {
-          await createCountersTable()
-          await createDefaultCounters()
-        } catch (e) {
-          console.log(e instanceof Error ? e.message : e)
-        }
-      } else {
-        console.log(`Connection to \x1b[96m${Deno.env.get('PG_DB')}\x1b[0m was failed:`, error.message)
+        console.log('No counters table found: creating one...')
+        return await createCountersTable()
       }
+      return 0
     })
-}
 
-async function createUsersTable() {
-  console.log('No users table found: creating one...')
+const createUsersTable = () =>
+  db.pool.query(initTableUsers).then(() => {
+    console.log('Create users table: Success!')
+  }).catch((err) => {
+    console.log('Create users table: Failed:', err.message)
+  })
 
-  await db.pool.query(/* sql */ `
-      DO $$ BEGIN
-        CREATE TYPE role AS ENUM ('admin', 'user', 'guest');
-      EXCEPTION
-        WHEN duplicate_object THEN null;
-      END $$;
-
-      CREATE TABLE IF NOT EXISTS users (
-        id serial PRIMARY KEY,
-        name varchar(25) NOT NULL UNIQUE,
-        email varchar(25),
-        role role NOT NULL DEFAULT 'user',
-        token varchar(256) NOT NULL,
-        created_at timestamptz NOT NULL DEFAULT now(),
-        updated_at timestamptz NOT NULL DEFAULT now(),
-        created_by varchar(25) NOT NULL DEFAULT 'default'
-      );`)
-}
-
-async function createDefaultUser() {
+const createDefaultUser = async () => {
   console.log('Creating default user...')
 
   const token = await createAdminUserToken()
   if (token) {
-    const { rows } = await db.pool.query(/* sql */ `
+    db.pool.query(/* sql */ `
       INSERT INTO users (name, role, token) VALUES ('${
       Deno.env.get('ADMIN_NAME') ?? 'admin'
-    }', 'admin', '${token}') RETURNING *;`)
-    if (rows.length) console.log('Success!')
+    }', 'admin', '${token}') RETURNING *;`).then(({ rows }) => {
+      if (rows.length) console.log('Create default user: Success!')
+    }).catch((err) => console.log('Create default user: Failed:', err.message))
   }
 }
 
-function checkUsersTable() {
+const checkUsersTable = () =>
   db.pool
     .query<Record<'name', string>>('SELECT name FROM users;')
     .then(({ rows }) => {
       if (!rows.length) createDefaultUser()
       else console.log('Available users:', rows.map(({ name }) => name).join(', '))
+      return 0
     })
-    .catch(async (error) => {
+    .catch((error) => {
       if (error.code === ERROR_CODES.relation) {
-        try {
-          await createUsersTable()
-          await createDefaultUser()
-        } catch (e) {
-          console.log(e instanceof Error ? e.message : e)
-        }
+        console.log('No users table found: creating one...')
+        createUsersTable().then(() => createDefaultUser())
+        return 1
       } else {
         console.log(`Connection to \x1b[96m${Deno.env.get('PG_DB')}\x1b[0m was failed:`, error.message)
       }
+      return 0
     })
-}
 
-export default function checkTables() {
-  checkUsersTable()
-  checkCountersTable()
+export default async function checkTables() {
+  const changes = await checkUsersTable() + await checkCountersTable()
+  if (changes) {
+    db.pool.query(initTriggers).then(() => {
+      console.log('Create database triggers: Success!')
+    }).catch((err) => {
+      console.log('Create database triggers: Failed:', err.message)
+    })
+  }
 }
---
+++ server/db/init.ts
@@ -0,0 +1,49 @@
+export const initTableCounters = /* sql */ `
+CREATE TABLE IF NOT EXISTS counters (
+  id serial PRIMARY KEY,
+  name varchar(25) NOT NULL UNIQUE,
+  value int4 NOT NULL DEFAULT 0,
+  updated_at timestamptz NOT NULL DEFAULT now(),
+  created_at timestamptz NOT NULL DEFAULT now(),
+  created_by varchar(25) NOT NULL DEFAULT 'default',
+  creator_id int4 NOT NULL DEFAULT 0
+);
+
+INSERT INTO counters (name) VALUES ('default'), ('counter-example') ON CONFLICT DO NOTHING RETURNING *;`
+
+export const initTableUsers = /* sql */ `
+DO $$ BEGIN
+  CREATE TYPE role AS ENUM ('admin', 'user', 'guest');
+EXCEPTION
+  WHEN duplicate_object THEN null;
+END $$;
+
+CREATE TABLE IF NOT EXISTS users (
+  id serial PRIMARY KEY,
+  name varchar(25) NOT NULL UNIQUE,
+  email varchar(25),
+  role role NOT NULL DEFAULT 'user',
+  token varchar(256) NOT NULL,
+  updated_at timestamptz NOT NULL DEFAULT now(),
+  created_at timestamptz NOT NULL DEFAULT now(),
+  created_by varchar(25) NOT NULL DEFAULT 'default',
+  creator_id int4 NOT NULL DEFAULT 0
+);`
+
+export const initTriggers = /* sql */ `
+CREATE OR REPLACE FUNCTION on_update()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at := now();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trg_counters_on_update
+BEFORE UPDATE OF value ON counters
+FOR EACH ROW WHEN (OLD.value IS DISTINCT FROM NEW.value)
+EXECUTE FUNCTION on_update();
+
+CREATE OR REPLACE TRIGGER trg_users_on_update
+BEFORE UPDATE ON users
+FOR EACH ROW EXECUTE FUNCTION on_update();`
